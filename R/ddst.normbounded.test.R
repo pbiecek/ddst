eecddb27614d9a746ee897459ede8a9635c258d7
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2019-10-01 22:29:26 +0200

    fixed examples

--- R/ddst.normbounded.test.R
+++ R/ddst.normbounded.test.R
@@ -43,17 +43,17 @@
 #' # for given vector of 19 numbers
 #' z <- c(13.41, 6.04, 1.26, 3.67, -4.54, 2.92, 0.44, 12.93, 6.77, 10.09,
 #'     4.10, 4.04, -1.97, 2.17, -5.38, -7.30, 4.75, 5.63, 8.84)
-#' ddst.norm.test(z, compute.p=TRUE)
+#' ddst.normbounded.test(z, compute.p=TRUE)
 #'
 #' # H0 is true
 #' z <- rnorm(80)
-#' t <- ddst.norm.test(z, compute.p=TRUE)
+#' t <- ddst.normbounded.test(z, compute.p=TRUE)
 #' t
 #' plot(t)
 #'
 #' # H0 is false
 #' z <- rexp(80,4)
-#' t <- ddst.norm.test(z, B=5000, compute.p=TRUE)
+#' t <- ddst.normbounded.test(z, B=5000, compute.p=TRUE)
 #' t
 #' plot(t)
 #'
@@ -68,7 +68,7 @@
 #'       -1.759923, -1.786519, -1.726779, -1.738528, -1.754345, -1.781646,
 #'       -1.641949, -1.755936, -1.775175, -1.736956, -1.705103, -1.743255,
 #'       -1.82613, -1.826967, -1.780025, -1.684504, -1.751168)
-#' t <- ddst.norm.test(z, compute.p=TRUE)
+#' t <- ddst.normbounded.test(z, compute.p=TRUE)
 #' t
 #' plot(t)
 `ddst.normbounded.test` <-
